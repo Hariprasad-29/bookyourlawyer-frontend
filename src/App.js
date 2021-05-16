ac504852e71754d0267505c99403aaf60f6ab60e
HARIPRASAD P <hariprasad.p@gogosoon.com>
2021-05-16 22:48:20 +0530

    Added AppBar in ClientLandingPage

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ function App() {
     <div className="App">
       {/* <Login />
       <Register /> */}
-      {/* <LawyerLandingPage/> */}
-      <ClientLandingPage/>
+      <LawyerLandingPage/>
+      {/* <ClientLandingPage/> */}
     </div>
   );
 }
